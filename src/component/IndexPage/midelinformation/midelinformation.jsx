c95be90a0e10945ac392d0e9d4775e6004560a2f
alisafaa123 <alisafaa.ve911@gmail.com>
2024-11-11 17:27:29 +0300

    v 3.1

--- src/component/IndexPage/midelinformation/midelinformation.jsx
+++ src/component/IndexPage/midelinformation/midelinformation.jsx
@@ -1,6 +1,8 @@
 
 import './midelinformation.css';
 import Userinf from '../../../assets/userimg.svg';
+import InsertProject from '../../../assets/insertProject.svg';
+import Dotes from '../../../assets/dotes.svg';
 
 const Midelinformation = () => {
     return(
@@ -9,7 +11,22 @@ const Midelinformation = () => {
                         <img src={Userinf} alt="" />
                         <input type="text" placeholder='Mustafa Let’s Create a Project !'/>
                         <div className="insertimg">
-                                <img src="" alt="" />
+                                <img src={InsertProject} alt="" />
+                        </div>
+                    </div>
+                    <div className="postsharing">
+                        <div className="sender">
+                                <div className="info">
+                                <div className="imgsender"></div>
+                                        <h1>Client Name</h1>
+                                        <p>Posted 2 hours ago  </p>
+                                </div>
+                                <div className="otheroptionPost">
+                                        <button>Available</button>
+                                        <button>
+                                                <img src={Dotes} alt="" />
+                                        </button>
+                                </div>
                         </div>
                     </div>
             </div>
