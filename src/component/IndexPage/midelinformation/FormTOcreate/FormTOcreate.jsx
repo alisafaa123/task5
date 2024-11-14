63b7d7c9dfe344c8406bafb929e2122470e965d6
alisafaa123 <alisafaa.ve911l@gmail.com>
2024-11-14 15:16:57 +0300

    v 4.1.2

--- src/component/IndexPage/midelinformation/FormTOcreate/FormTOcreate.jsx
+++ src/component/IndexPage/midelinformation/FormTOcreate/FormTOcreate.jsx
@@ -1,20 +1,28 @@
 import './FormTOcreate.css';
-import ButtonHideForm from '../../../../assets/buttonHideForm.svg'
-const FormTOcreate = () => { 
-    return(
-        <div className="FormtocreateProject">
-                <div className="insertForm">
-                        <div className="title">
-                            <h1>Create a project</h1>
-                            <button><img src={ButtonHideForm} alt="" /></button>
-                        </div>
-                        <div className="headerOfofrm">
-                            <div className="tow"></div>
-                            <div className="one"></div>
-                        </div>
+import ButtonHideForm from '../../../../assets/buttonHideForm.svg';
+import { useEffect } from 'react';
+
+const FormTOcreate = ({ FlageForm, onClose }) => {
+    useEffect(() => {
+        console.log(`Form visibility changed: ${FlageForm}`);
+    }, [FlageForm]);
+
+    return (
+        <div className="FormtocreateProject" style={{ display: FlageForm ? 'block' : 'none' }}>
+            <div className="insertForm">
+                <div className="title">
+                    <h1>Create a project</h1>
+                    <button onClick={onClose}>
+                        <img src={ButtonHideForm} alt="Hide Form" />
+                    </button>
                 </div>
+                <div className="headerOfofrm">
+                    <div className="tow"></div>
+                    <div className="one"></div>
+                </div>
+            </div>
         </div>
     );
 }
-export default FormTOcreate;
 
+export default FormTOcreate;
--- src/component/IndexPage/midelinformation/midelinformation.jsx
+++ src/component/IndexPage/midelinformation/midelinformation.jsx
@@ -9,121 +9,79 @@ import LIkeicon from '../../../assets/LIkeicon.svg';
 import LIkeiconred from '../../../assets/LIkeiconred.svg';
 import CreateForm from './FormTOcreate/FormTOcreate';
 
-
 const Midelinformation = () => {
-        const [FlageForm , setFlageForm] = useState(false);
-        const [Flage , setFlage] = useState(false);
-        const [Flage1 , setFlage1] = useState(false);
-        const [Flage2 , setFlage2] = useState(false);
-        const Title1 = "to build a responsive, user-focused web application. Must be skilled in both front-end and back-end development...";
-        const Title2 = "to build a responsive, user-focused web application. Must be skilled in both front-end to build a responsive, user-focused web application. Must be skilled in both front-end and back-end developm to build a responsive, user-focused web application. Must be skilled in both front-end and back-end developm and back-end development...";
-        const ChangeFlage =() =>{
-                setFlage(!Flage)
-        };
-        const ChangeFlage1 =() =>{
-                setFlage1(!Flage1)
-        };
-        const ChangeFlage2 =() =>{
-                setFlage2(!Flage2)
-        };
-        const ChangeFlageForm =() =>{
-                setFlageForm(!FlageForm)
-        };
-        return(
-            <div className="Midelinformation">
-                    <div className="createproject">
-                        <img src={Userinf} alt="" />
-                        <button onClick={ChangeFlageForm}>
-                                  Mustafa Let’s Create a Project !
-                                  <div className="insertimg">
-                                <img src={InsertProject} alt="" />
-                                 </div>
+    const [FlageForm, setFlageForm] = useState(false);
+    const [Flage, setFlage] = useState(false);
+    const [Flage1, setFlage1] = useState(false);
+    const [Flage2, setFlage2] = useState(false);
+
+    const Title1 = "to build a responsive, user-focused web application. Must be skilled in both front-end and back-end development...";
+    const Title2 = "to build a responsive, user-focused web application. Must be skilled in both front-end and back-end development and have good experience in modern frameworks.";
+
+    const toggleFlage = () => setFlage(!Flage);
+    const toggleFlage1 = () => setFlage1(!Flage1);
+    const toggleFlage2 = () => setFlage2(!Flage2);
+    const toggleFlageForm = () => setFlageForm(!FlageForm);
+
+    return (
+        <div className="Midelinformation">
+            <div className="createproject">
+                <img src={Userinf} alt="" />
+                <button onClick={toggleFlageForm}>
+                    Mustafa Let’s Create a Project!
+                    <div className="insertimg">
+                        <img src={InsertProject} alt="" />
+                    </div>
+                </button>
+            </div>
+
+            <div className="postsharing">
+                <div className="sender">
+                    <div className="info">
+                        <div className="imgsender"></div>
+                        <div className="textuser">
+                            <h1>Client Name</h1>
+                            <p>Posted 2 hours ago</p>
+                        </div>
+                    </div>
+                    <div className="otheroptionPost">
+                        <button className="Available">Available</button>
+                        <button className="Dotes">
+                            <img src={Dotes} alt="" />
                         </button>
                     </div>
-                    <div className="postsharing">
-                        <div className="sender">
-                                <div className="info">
-                                        <div className="imgsender"></div>
-                                              <div className="textuser">
-                                                  <h1>Client Name</h1>
-                                                  <p>Posted 2 hours ago  </p>
-                                              </div>
-                                 </div>
-                                 <div className="otheroptionPost">
-                                                <button className='Available'>Available</button>
-                                                <button className='Dotes'>
-                                                        <img src={Dotes} alt="" />
-                                                </button>
-                                </div>
+                </div>
+
+                <div className="postDetails">
+                    <div className="title">
+                        <h1>Looking for Full-Stack Developer <br /> with experience +2 years</h1>
+                        <p style={{ display: Flage ? 'none' : 'block' }}>
+                            {Title1} <button onClick={toggleFlage}>See more</button>
+                        </p>
+                        <p style={{ display: Flage ? 'block' : 'none' }}>
+                            {Title2} <button onClick={toggleFlage}>See less</button>
+                        </p>
+                    </div>
+                    <img src={PostImg} alt="" />
+                    <div className="action">
+                        <div className="actionoption">
+                            <button onClick={toggleFlage1}>
+                                <img style={{ display: Flage1 ? 'none' : 'block' }} src={LIkeicon} alt="" />
+                                <img style={{ display: Flage1 ? 'block' : 'none' }} src={LIkeiconred} alt="" />
+                            </button>
+                            <p>like</p>
                         </div>
-                        <div className="postDetails">
-                                 <div className="title">
-                                     <h1>Looking for Full-Sack Developer <br /> with experience +2 years</h1>
-                                     <p style={{display: Flage ? 'none' : 'block'}}>{Title1}<button onClick={ChangeFlage}>See more</button></p>
-                                     <p style={{display: Flage ? 'block' : 'none'}}>{Title2}<button onClick={ChangeFlage}>See less</button></p>
-                                 </div>
-                                 <img src={PostImg} alt="" />
-                                 <div className="action">
-                                        <div className="actionoption" >
-                                               <button onClick={ChangeFlage1}>
-                                                 <img style={{display: Flage1 ? 'none' : 'block'}} src={LIkeicon} alt="" />
-                                                 <img style={{display: Flage1 ? 'block' : 'none'}} src={LIkeiconred} alt="" />
-                                               </button>
-                                                <p>like</p>
-                                        </div>
-                                        <div className="actionoption">
-                                                <img src={MessageIcon} alt="" />
-                                                <p>comment</p>
-                                        </div>
-                                 </div>
+                        <div className="actionoption">
+                            <img src={MessageIcon} alt="" />
+                            <p>comment</p>
                         </div>
                     </div>
-                    <div className="postsharing">
-                        <div className="sender">
-                                <div className="info">
-                                        <div className="imgsender"></div>
-                                              <div className="textuser">
-                                                  <h1>Client Name</h1>
-                                                  <p>Posted 2 hours ago  </p>
-                                              </div>
-                                 </div>
-                                 <div className="otheroptionPost">
-                                                <button className='Available'>Available</button>
-                                                <button className='Dotes'>
-                                                        <img src={Dotes} alt="" />
-                                                </button>
-                                </div>
-                        </div>
-                        <div className="postDetails">
-                                 <div className="title">
-                                     <h1>Looking for Full-Sack Developer <br /> with experience +2 years</h1>
-                                     <p>to build a responsive, user-focused web application. Must be skilled in both front-end and back-end development</p>
-                                 </div>
-                                 <div className="title">
-                                     <h1>Duration of project</h1>
-                                     <p>4 Months</p>
-                                 </div>
-                                 <div className="title">
-                                     <h1>Pricing</h1>
-                                     <p>Hourly  $ 50</p>
-                                 </div>
-                                 <div className="action">
-                                        <div className="actionoption">
-                                                <button onClick={ChangeFlage2}>
-                                                        <img style={{display: Flage2 ? 'none' : 'block'}} src={LIkeicon} alt="" />
-                                                        <img style={{display: Flage2 ? 'block' : 'none'}} src={LIkeiconred} alt="" />
-                                               </button>
-                                                <p>like</p>
-                                        </div>
-                                        <div className="actionoption">
-                                                <img src={MessageIcon} alt="" />
-                                                <p>comment</p>
-                                        </div>
-                                 </div>
-                        </div>
-                    </div>
-                    <CreateForm  style={{display: FlageForm ? 'none' : 'block'}}/>
+                </div>
             </div>
+
+            <CreateForm FlageForm={FlageForm} onClose={toggleFlageForm} />
+        </div>
     );
 }
+
 export default Midelinformation;
